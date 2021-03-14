d10350d56f5ba5c34e089bbe7b0f13874a9cac11
John Herrlin <john.herrlin@fortnox.se>
2021-03-14 17:46:13 +0100

    cleanup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,1 @@
-((nil . ((cider-default-cljs-repl . shadow)
-         (cider-preferred-build-tool . shadow-cljs)
-         (cider-clojure-cli-global-options . "-A:develop")
-         (cider-shadow-default-options . ":app"))))
+((nil . ((cider-clojure-cli-global-options . "-A:develop"))))
